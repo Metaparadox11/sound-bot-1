7a866b7ae3b687e23463ba6dfd29e6f57f96607e
Metaparadox11 <metaparadox11@gmail.com>
2021-03-13 14:26:07 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -23,8 +23,9 @@ module.exports = {
           // Play audio on the broadcast
           const dispatcher = broadcast.play(file_path);
 
-          let voiceChannel = member.guild.channels.cache.find(channel => channel.name === args[0]);
+          let voiceChannel = member.guild.channels.cache.find(voiceChannel => voiceChannel.name === args[0]);
           if (!voiceChannel) return message.reply(`The channel ${args[0]} does not exist!`);
+          if (voiceChannel.type !== 'voice') return message.reply(`That channel isn't a voice channel.`);
           const connection = await voiceChannel.join();
 
           // Play this broadcast across multiple connections (subscribe to the broadcast)
